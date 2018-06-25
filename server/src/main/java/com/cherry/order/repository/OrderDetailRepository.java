abc8ed97e150dedb1b6f9933462bfd34cfabbcea
cherry-peng <xiangshaopeng@doctorwork.com>
2018-06-25 16:52:11 +0800

    保存订单详情

--- server/src/main/java/com/cherry/order/repository/OrderDetailRepository.java
+++ server/src/main/java/com/cherry/order/repository/OrderDetailRepository.java
@@ -1,9 +1,12 @@
 package com.cherry.order.repository;
 
+import com.cherry.order.entity.OrderDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * Description:
  * Author: cherry
  * Date: Created in 2018/6/22 下午9:48
  */
-public interface OrderDetailRepository {
+public interface OrderDetailRepository extends JpaRepository<OrderDetail,String> {
 }
--- server/src/main/java/com/cherry/order/service/impl/OrderServiceImpl.java
+++ server/src/main/java/com/cherry/order/service/impl/OrderServiceImpl.java
@@ -2,9 +2,11 @@ package com.cherry.order.service.impl;
 
 import com.cherry.order.dto.OrderDetailDTO;
 import com.cherry.order.dto.OrderMasterDTO;
+import com.cherry.order.entity.OrderDetail;
 import com.cherry.order.entity.OrderMaster;
 import com.cherry.order.enums.OrderEnum;
 import com.cherry.order.enums.PayEnum;
+import com.cherry.order.repository.OrderDetailRepository;
 import com.cherry.order.repository.OrderMasterRepository;
 import com.cherry.order.service.OrderService;
 import com.cherry.order.utils.CherryUtil;
@@ -12,6 +14,7 @@ import com.cherry.product.client.ProductClient;
 import com.cherry.product.dto.DecreaseStockDTO;
 import com.cherry.product.dto.ProductInfoDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -35,6 +38,9 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderMasterRepository orderMasterRepository;
 
+    @Autowired
+    private OrderDetailRepository orderDetailRepository;
+
     @Override
     public String createOrder(OrderMasterDTO orderDTO) {
 
@@ -47,10 +53,20 @@ public class OrderServiceImpl implements OrderService {
         BigDecimal totalPrice = BigDecimal.ZERO;
         List<OrderDetailDTO> orderDetailList = orderDTO.getItems();
 
+        OrderMaster orderMaster = new OrderMaster();
+        orderMaster.setOrderId(CherryUtil.generateUUID());
+
+        //循环购买的商品列表计算总价，并保存订单详情
         for (OrderDetailDTO orderDetailDTO : orderDetailList) {
             for (ProductInfoDTO product : products) {
                 if(orderDetailDTO.getProductId().equals(product.getProductId())){
                     totalPrice = totalPrice.add(product.getProductPrice().multiply(new BigDecimal(orderDetailDTO.getProductQuantity())));
+                    OrderDetail orderDetail = new OrderDetail();
+                    BeanUtils.copyProperties(product,orderDetail);
+                    orderDetail.setOrderId(orderMaster.getOrderId());
+                    orderDetail.setDetailId(CherryUtil.generateUUID());
+                    orderDetail.setProductQuantity(orderDetailDTO.getProductQuantity());
+                    orderDetailRepository.save(orderDetail);
                 }
             }
         }
@@ -58,12 +74,10 @@ public class OrderServiceImpl implements OrderService {
         log.info("购买的商品总价：{}",totalPrice);
 
         //3.扣库存
-        List<ProductInfoDTO> productInfoDTOList = productClient.decreaseStockForOrder(orderDetailList.stream()
+        productClient.decreaseStockForOrder(orderDetailList.stream()
                 .map(e -> new DecreaseStockDTO(e.getProductId(),e.getProductQuantity())).collect(Collectors.toList()));
         //4.订单入库
         //insert OrderMaster
-        OrderMaster orderMaster = new OrderMaster();
-        orderMaster.setOrderId(CherryUtil.generateUUID());
         orderMaster.setBuyerName(orderDTO.getName());
         orderMaster.setBuyerPhone(orderDTO.getPhone());
         orderMaster.setBuyerAddress(orderDTO.getAddress());
@@ -72,7 +86,6 @@ public class OrderServiceImpl implements OrderService {
         orderMaster.setOrderStatus(OrderEnum.NEW.getCode());
         orderMaster.setPayStatus(PayEnum.WAIT.getCode());
         orderMasterRepository.save(orderMaster);
-        //insert OrderDetail TODO
         return orderMaster.getOrderId();
     }
 }
