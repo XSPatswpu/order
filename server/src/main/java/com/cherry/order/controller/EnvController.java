e7772dda7306fe894f1e69854c62f832bd397d8a
cherry-peng <xiangshaopeng@doctorwork.com>
2018-07-01 15:45:45 +0800

    order-server 引入spring-cloud-bus依赖，实现配置自动刷新

---
+++ server/src/main/java/com/cherry/order/controller/EnvController.java
@@ -0,0 +1,25 @@
+package com.cherry.order.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Description:
+ * Author: cherry
+ * Date: Created in 2018/6/29 上午10:16
+ */
+@RequestMapping("/env")
+@RestController
+@RefreshScope
+public class EnvController {
+    @Value("${env}")
+    private String env;
+
+    @GetMapping("/print")
+    public String getEvn(){
+        return env;
+    }
+}
